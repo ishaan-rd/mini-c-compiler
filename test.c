a30c3cd0370a01ee858eb08911895d127a3e0bc1
26prajval98 <26prajval98@gmail.com>
2019-01-17 23:23:09 +0530

    jatre

--- test.c
+++ test.c
@@ -1,16 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
-#define MAX 100*/
+#  include <stdio.h>
+#  include <stdlib.h>
+#define MAX 100
 
 void main() {
     int a=5;
     int b=3;
     int c=a%b;
-
-    printf ("a mod b = %d\n", c);
-    /*sjkdl
-    /*
-    */
+    char * X = "ashdis\"abd";
     
 
    /** alskdfj
