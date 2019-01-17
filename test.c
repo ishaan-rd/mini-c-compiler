bdc59b4c8fe8984c2fcf9f56841491b70999210e
26prajval98 <26prajval98@gmail.com>
2019-01-18 01:43:33 +0530

    All done

--- test.c
+++ test.c
@@ -1,24 +1,5 @@
 #  include <stdio.h>
 #  include <stdlib.h>
-#define MAX 100
-
-
-void main() {
-    int a=55.5;
-    int b=3;
-    int c=a%b;
-    char x = 'x';
-    
-    printf("Hello World");    
-
-   /** alskdfj
-    * laksdfj
-
-
-   /*
-   kdsjfkl
-   kjdfks
-   */*/
-   
-    return;
+int main(){
+    int 8p = 10;
 }
