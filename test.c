de04a8463bf6a1a5a04151ab5c6eaf19a95001c4
26prajval98 <26prajval98@gmail.com>
2019-01-18 00:10:03 +0530

    Revert "added color"

--- test.c
+++ test.c
@@ -1,12 +1,22 @@
-#include <stdio.h>
-#include <stdlib.h>
-#define MAX 100*/
+#  include <stdio.h>
+#  include <stdlib.h>
+#define MAX 100
 
 void main() {
     int a=5;
     int b=3;
     int c=a%b;
-    char x[] = "ashdis\"abd";
-    printf(" aishbdiysayd asadsa");
+    char * X = "ashdis\"abd";
+    
+
+   /** alskdfj
+    * laksdfj
+
+
+   /*
+   kdsjfkl
+   kjdfks
+   */*/
+   
     return;
 }
