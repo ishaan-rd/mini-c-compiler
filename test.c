7dd91ed606f705f5c0e40faeeee547f598af3784
26prajval98 <26prajval98@gmail.com>
2019-01-18 00:33:55 +0530

    added string, char, int, float

--- test.c
+++ test.c
@@ -1,14 +1,15 @@
 #  include <stdio.h>
 #  include <stdlib.h>
 #define MAX 100
-*/
+
 
 void main() {
-    int a=5;
+    int a=55.5;
     int b=3;
     int c=a%b;
-    char * X = "ashdis\"abd";
+    char x = 'x';
     
+    printf("Hello World");    
 
    /** alskdfj
     * laksdfj
