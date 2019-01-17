f48dbcf8bb688ff0c9b10f5c730339acababac5f
26prajval98 <26prajval98@gmail.com>
2019-01-18 00:03:00 +0530

    added color

--- test.c
+++ test.c
@@ -1,22 +1,12 @@
-#  include <stdio.h>
-#  include <stdlib.h>
-#define MAX 100
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 100*/
 
 void main() {
     int a=5;
     int b=3;
     int c=a%b;
-    char * X = "ashdis\"abd";
-    
-
-   /** alskdfj
-    * laksdfj
-
-
-   /*
-   kdsjfkl
-   kjdfks
-   */*/
-   
+    char x[] = "ashdis\"abd";
+    printf(" aishbdiysayd asadsa");
     return;
 }
